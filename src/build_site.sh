382c970e5f7a1d7599a8745bdb7d903fbc39325c
david watson <davidthewatson@gmail.com>
2022-07-25 17:44:30 -0400

    Add separate static jinja script for resume
    The full story is this:
    
    https://en.wikipedia.org/wiki/Wikipedia:Emerson_and_Wilde_on_consistency
    
    The foolish consistency for me was that the resume was being processed
    with everything else. It's a foolish consistency because if you think
    about it, the confines of a resume aren't really the same as most other
    general purpose writing. I wrestled with this mistake for about a year
    before I realized my mistake. Today I fixed it, and while the fix isn't
    perfect, it's dramatically better than what I had before.
    
    I'll make a few tweaks in the coming days, but for now, I just needed
    to watch that the main script didn't delete the whole thing!

--- src/build_site.sh
+++ src/build_site.sh
@@ -5,24 +5,16 @@ echo $SITEROOT
 
 while inotifywait -r -e close_write -e move -e create $SRC $STATIC
 do
-    rm -rf $DOCS/*
+#    rm -rf $DOCS/*
     cp -rf $STATIC/* $DOCS/.
     echo "$STATIC/* $DOCS/."
     
     python ./make_index.py
 
     python ./md2html.py
-    python ./md2txt.py
-    python ./html2pdf.py
 
     echo "HTML STATS"
     find $DOCS -name "*.html"
     find $DOCS -name "*.html" | wc -l
-    echo "TEXT STATS"
-    find $DOCS -name "*.txt"
-    find $DOCS -name "*.txt" | wc -l
-    echo "PDF STATS"
-    find $DOCS -name "*.pdf"
-    find $DOCS -name "*.pdf" | wc -l
 done
 
