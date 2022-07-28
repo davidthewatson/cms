c66faff3f9ad90abd7d069635a5c63767c946b4d
david watson <davidthewatson@gmail.com>
2022-07-28 14:51:48 -0400

    Fix PDF generation
    Note to self:
    
    There is a bug in the wkhtmltopdf minor release prior to the AUR package
    listed here. The difference is the -static postfix in the name of the
    package. Caveat emptor!
    
    wkhtmltopdf-static                                     [Installed] 0.12.6-2  AUR

--- src/build_site.sh
+++ src/build_site.sh
@@ -2,10 +2,12 @@
 
 trap ctrl_c INT
 
+
 function ctrl_c ()
 {
     echo "Exiting..."
     python ./resumemd2html.py 
+    wkhtmltopdf -B 2.54cm -L 2.54cm  -R 2.54cm -T 2.54cm http://localhost:8000/resume/index.html $DOCS/resume/index.pdf
     exit 2
 }
 
@@ -19,8 +21,9 @@ do
     echo "$STATIC/* $DOCS/."
     
     python ./make_index.py
-
     python ./md2html.py
+    python ./resumemd2html.py 
+    wkhtmltopdf -B 2.54cm -L 2.54cm  -R 2.54cm -T 2.54cm http://localhost:8000/resume/index.html $DOCS/resume/index.pdf
 
     echo "HTML STATS"
     find $DOCS -name "*.html"
