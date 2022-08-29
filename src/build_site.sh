ae159a9cc10298a648f2d25ba443f83233220488
david watson <davidthewatson@gmail.com>
2022-08-29 12:07:27 -0400

    Add delete event to inotify

--- src/build_site.sh
+++ src/build_site.sh
@@ -7,14 +7,14 @@ function ctrl_c ()
 {
     echo "Exiting..."
     python ./resumemd2html.py 
-    wkhtmltopdf -B 3cm -L 2cm  -R 2cm -T 2cm http://localhost:8000/resume/index.html $DOCS/resume/index.pdf
+    wkhtmltopdf -B 3cm -L 2cm  -R 2cm -T 2cm http://localhost:8000/resume/david_watson/index.html $DOCS/resume/david_watson/index.pdf
     exit 2
 }
 
 # you should source .env from you siteroot such that it is defined before running this script
 echo $SITEROOT
 
-while inotifywait -r -e close_write -e move -e create $SRC $STATIC
+while inotifywait -r -e close_write -e move -e create -e delete $SRC $STATIC
 do
 #    rm -rf $DOCS/*
     cp -rf $STATIC/* $DOCS/.
@@ -23,7 +23,7 @@ do
     python ./make_index.py
     python ./md2html.py
     python ./resumemd2html.py 
-    wkhtmltopdf -B 3cm -L 2cm  -R 2cm -T 2cm http://localhost:8000/resume/index.html $DOCS/resume/index.pdf
+    wkhtmltopdf -B 3cm -L 2cm  -R 2cm -T 2cm http://localhost:8000/resume/david_watson/index.html $DOCS/resume/david_watson/index.pdf
 
     echo "HTML STATS"
     find $DOCS -name "*.html"
