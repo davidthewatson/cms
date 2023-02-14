855152c7df28b8fdae3159eaf58a7e0f900c8e72
watson <watson@acer-swift-silver.watsonian.org>
2023-02-14 12:18:07 -0500

    Add Fedora 38 Sway spin

---
+++ install_fedora.sh
@@ -0,0 +1,9 @@
+cd ../cms/src
+sudo dnf install wkhtmltopdf inotify-tools
+
+python -m venv .venv
+source .venv/bin/activate
+# pip install wheel
+# python setup.py bdist_wheel
+pip install -r requirements.txt
+deactivate
