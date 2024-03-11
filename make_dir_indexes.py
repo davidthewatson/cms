2b19d16c9bc5fbeb58a731d94c6203a615690eed
david watson <davidthewatson@gmail.com>
2024-03-10 22:17:48 -0400

    Fix header

--- make_dir_indexes.py
+++ make_dir_indexes.py
@@ -6,26 +6,18 @@ load_dotenv()
 SRC = os.environ['SRC']
 DOCS = os.environ['DOCS']
 
-# Get the input directory
-input_directory = SRC
-
-# Get all markdown files in the input directory
-markdown_files = glob.glob(os.path.join(input_directory, "**/*.md"))
-
-# Create the index file
+markdown_files = glob.glob(os.path.join(SRC, "**.md"))
+print(markdown_files)
 index_file = open("index.md", "w")
 
-# Write the header to the index file
-index_file.write("# Index\n\n")
+index_file.write("# Welcome!\n\n")
 
-# Iterate over all markdown files
-for markdown_file in markdown_files:
-
-    # Get the file name
+for markdown_file in sorted(markdown_files):
+    if markdown_file.find('index') > -1:
+        continue
     file_name = os.path.basename(markdown_file)
+    title = file_name[:-3].title().replace('_', ' ')
+    file_name = file_name.replace('.md', '.html')
+    index_file.write(f"* [{title}]({file_name})\n")
 
-    # Write a link to the file to the index file
-    index_file.write(f"* [{file_name}]({file_name})\n")
-
-# Close the index file
 index_file.close()
