9d0db98d89b2859ade2323e5f2bebb6bc62fb187
david watson <davidthewatson@gmail.com>
2024-03-10 22:05:10 -0400

    Fix shell handling from zsh to fish

--- install/install_arch.sh
+++ install/install_arch.sh
@@ -1,5 +1,5 @@
-sudo pamac install codespell inotify-tools uv
-uv venv
-. ./.venv/bin/activate
+sudo pamac install codespell inotify-tools proselint uv
+uv venv ../.venv
+source ../.venv/bin/activate
 uv pip install -r requirements.txt
 deactivate
